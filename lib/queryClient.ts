30e0466e6e07024f9e63a04c639340e902b42169
Henry Zhu <hi@henryzoo.com>
2025-05-19 17:00:14 -0400

    fix: storage warning

--- lib/queryClient.ts
+++ lib/queryClient.ts
@@ -1,10 +1,28 @@
 import { QueryClient } from '@tanstack/react-query';
 import { experimental_createQueryPersister } from '@tanstack/react-query-persist-client';
+import type { AsyncStorage } from '@tanstack/query-persist-client-core';
+
+const localStorageWrapper: AsyncStorage = {
+  getItem: async (key: string) => {
+    return Promise.resolve(window.localStorage.getItem(key));
+  },
+  setItem: async (key: string, value: string) => {
+    window.localStorage.setItem(key, value);
+    return Promise.resolve();
+  },
+  removeItem: async (key: string) => {
+    window.localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+  entries: async () => {
+    return Promise.resolve(Object.entries(window.localStorage));
+  },
+};
 
 export const appPersister = experimental_createQueryPersister({
-  storage: window.localStorage,
+  storage: localStorageWrapper,
   prefix: 'bsky-search-experimental',
-  maxAge: 1000 * 60 * 60 * 24 * 7,
+  maxAge: 1000 * 60 * 60 * 24 * 7,  
 });
 
 export const queryClient = new QueryClient({
